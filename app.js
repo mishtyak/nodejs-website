26347c1a6e65cefcda62b56c1abb44d532f290b1
mishtiak <vintovkinm@gmail.com>
2020-07-19 21:50:36 +0700

    Проба внедрения PWA в проект

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const compression = require('compression');
+const serveStatic = require('serve-static');
 const path = require('path');
 const config = require(path.resolve('config'));
 const log = require(path.resolve('libs/winston'))(module);
@@ -9,9 +11,17 @@ app.engine('ejs', require('ejs-mate'));
 app.set('views', path.resolve('views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.resolve('public')));
+app.use(compression());
 
-app.get('/', (req, res, next) => {
+app.use(serveStatic('public', {
+    maxAge: '1d',
+    setHeaders(res, path) {
+        if (serveStatic.mime.lookup(path) === 'text/html')
+            res.setHeader('Cache-Control', 'no-cache, private')
+    }
+}));
+
+app.get('/', (req, res) => {
     res.render('index', {
         data: {
             h1: 'Hello!',
@@ -19,6 +29,33 @@ app.get('/', (req, res, next) => {
     });
 });
 
+app.get('/contacts', (req, res) => {
+    res.render('contacts', {
+        data: {
+            h1: 'Контакты',
+        }
+    });
+});
+
+app.use((req, res,) => {
+    log.error(`${new Date().toLocaleString()}. Error status: 404. URL: ${req.url}`);
+
+    res.status(404).render('errors/404', {
+        data: {
+            url: req.url
+        }
+    });
+});
+
+app.use((err, req, res, next) => {
+    if (res.headersSent)
+        return next(err);
+
+    log.error(`${new Date().toLocaleString()}. Error status: ${err.status || 500}. Message: ${err.message}. Stack: ${err.stack}`);
+
+    res.status(err.status || 500).render('errors/500', {err});
+});
+
 app.listen(config.get('port'), () => {
-    log.info(new Date().toLocaleString() + ': Server listening on port ' + config.get('port'));
+    log.info(new Date().toLocaleString() + '. Server listening on port ' + config.get('port'));
 });
--- public/app.js
+++ public/app.js
@@ -2,4 +2,48 @@ console.log(`app.js loaded (${Math.round(performance.now())})`);
 
 document.addEventListener('DOMContentLoaded', event => {
     console.log(`app.js event: ${event.type} (${Math.round(performance.now())})`);
+
+    [].forEach.call(document.querySelectorAll('.js_btn-pwa-install'), item => {
+        item.addEventListener('click', event => {
+            console.log('btn-pwa-install. Event: click');
+
+            if (!deferred_prompt) return;
+
+            deferred_prompt.prompt();
+
+            deferred_prompt.userChoice.then(result => {
+                console.log('userChoice: ', result);
+
+                if (result.outcome === 'accepted') {
+                    console.log('User accepted the install prompt');
+                } else {
+                    console.log('User dismissed the install prompt');
+                }
+
+                window.deferred_prompt = null;
+                event.target.hidden = true;
+            });
+        });
+    });
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', event => {
+        console.log(`event: ${event.type} (${Math.round(performance.now())})`);
+
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(reg => console.log(`[Service Worker] Registration (${Math.round(performance.now())})`))
+            .catch(err => console.log('err: ', err));
+    });
+}
+
+window.addEventListener('beforeinstallprompt', event => {
+    console.log('beforeinstallprompt: ', event);
+
+    event.preventDefault();
+    window.deferred_prompt = event;
+    [].forEach.call(document.querySelectorAll('.js_btn-pwa-install'), item => item.hidden = false);
+});
+
+window.addEventListener('appinstalled', event => console.log('appinstalled', event));
